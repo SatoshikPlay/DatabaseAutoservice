41e1ff9bee906707c7b635f50fe63697b0958aee
SatoshikPlay <vova23234243@gmail.com>
2021-01-11 18:09:37 +0400

    Добавление функции просмотра базы данных

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace DBAutoServise
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonEditingMode = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.LightGray;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 15);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1035, 442);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(177, 481);
+            this.buttonUpdate.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(262, 60);
+            this.buttonUpdate.TabIndex = 1;
+            this.buttonUpdate.Text = "Обновить";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.ButtonUpdate_Click);
+            // 
+            // buttonEditingMode
+            // 
+            this.buttonEditingMode.Location = new System.Drawing.Point(626, 481);
+            this.buttonEditingMode.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonEditingMode.Name = "buttonEditingMode";
+            this.buttonEditingMode.Size = new System.Drawing.Size(270, 60);
+            this.buttonEditingMode.TabIndex = 2;
+            this.buttonEditingMode.Text = "Режим редактирования";
+            this.buttonEditingMode.UseVisualStyleBackColor = true;
+            this.buttonEditingMode.Click += new System.EventHandler(this.ButtonEditingMode_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.buttonEditingMode);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainWindow";
+            this.Text = "Main window";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonEditingMode;
+    }
+}
---
+++ MainWindow.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBAutoServise
+{
+    public partial class MainWindow : Form
+    {
+        public string DataSource { get; private set; } = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Курсовая\DBAutoServise\DBAutoService.mdf;Integrated Security=True";
+        public SqlConnection Connection { get; set; } = null;
+        public SqlDataAdapter Adapter { get; set; } = null;
+        public DataTable Table { get; set; } = null;
+        public DataSet SetData { get; set; } = null;
+        public SqlCommand Command { get; set; } = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void ForbiddenInputs(KeyPressEventArgs e)
+        {
+            if (!char.IsPunctuation(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+        public void SearchCar(string text)
+        {
+            Command = new SqlCommand("SELECT * FROM CarDatabase WHERE Auto =  '%" + text + "%'", Connection);
+            Table = new DataTable();
+            Table.Load(Command.ExecuteReader());
+            dataGridView1.DataSource = Table.DefaultView;
+        }
+        public void DataLoad()
+        {
+            Adapter = new SqlDataAdapter("SELECT * FROM CarDatabase", Connection);
+            dataGridView1.DataSource = Table;
+            SetData = new DataSet();
+            Adapter.Fill(SetData, "CarDatabase");
+            dataGridView1.DataSource = SetData.Tables["CarDatabase"];
+        }
+        public void DataReload()
+        {
+            SetData.Tables["CarDatabase"].Clear();
+            Adapter.Fill(SetData, "CarDatabase");
+            dataGridView1.DataSource = SetData.Tables["CarDatabase"];
+        }
+        private void MainWindow_Load(object sender, EventArgs e)
+        { 
+            Connection = new SqlConnection(DataSource);
+            Connection.Open();
+            DataLoad();
+        }
+
+        private void ButtonUpdate_Click(object sender, EventArgs e)
+        {
+            DataReload();
+        }
+
+        private void ButtonEditingMode_Click(object sender, EventArgs e)
+        {
+            PassWordWindow passWordWindow = new PassWordWindow();
+            passWordWindow.Show();
+        }
+
+        private void ButtonSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBoxSeachCar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
