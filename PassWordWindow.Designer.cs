26364d6909fb8612e402a8b298fff768d646bb11
SatoshikPlay <vova23234243@gmail.com>
2021-01-11 18:14:22 +0400

    Добавленна функция проверки пароля

---
+++ PassWordWindow.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace DBAutoServise
+{
+    partial class PassWordWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelPass = new System.Windows.Forms.Label();
+            this.textBoxPass = new System.Windows.Forms.TextBox();
+            this.buttonPass = new System.Windows.Forms.Button();
+            this.labelError = new System.Windows.Forms.Label();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelPass
+            // 
+            this.labelPass.AutoSize = true;
+            this.labelPass.Location = new System.Drawing.Point(96, 22);
+            this.labelPass.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelPass.Name = "labelPass";
+            this.labelPass.Size = new System.Drawing.Size(114, 17);
+            this.labelPass.TabIndex = 0;
+            this.labelPass.Text = "Введите пароль";
+            // 
+            // textBoxPass
+            // 
+            this.textBoxPass.Location = new System.Drawing.Point(73, 42);
+            this.textBoxPass.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxPass.Name = "textBoxPass";
+            this.textBoxPass.PasswordChar = '*';
+            this.textBoxPass.Size = new System.Drawing.Size(169, 22);
+            this.textBoxPass.TabIndex = 1;
+            this.textBoxPass.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBoxPass_KeyPress);
+            // 
+            // buttonPass
+            // 
+            this.buttonPass.Location = new System.Drawing.Point(96, 113);
+            this.buttonPass.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonPass.Name = "buttonPass";
+            this.buttonPass.Size = new System.Drawing.Size(131, 38);
+            this.buttonPass.TabIndex = 2;
+            this.buttonPass.Text = "Подтвердить";
+            this.buttonPass.UseVisualStyleBackColor = true;
+            this.buttonPass.Click += new System.EventHandler(this.ButtonPass_Click);
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.BackColor = System.Drawing.SystemColors.Control;
+            this.labelError.ForeColor = System.Drawing.Color.Red;
+            this.labelError.Location = new System.Drawing.Point(96, 82);
+            this.labelError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(126, 17);
+            this.labelError.TabIndex = 3;
+            this.labelError.Text = "Неверный пароль";
+            this.labelError.Visible = false;
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(96, 159);
+            this.buttonCreate.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(131, 37);
+            this.buttonCreate.TabIndex = 4;
+            this.buttonCreate.Text = "Создать пароль";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.ButtonCreate_Click);
+            // 
+            // PassWordWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 223);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.buttonPass);
+            this.Controls.Add(this.textBoxPass);
+            this.Controls.Add(this.labelPass);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "PassWordWindow";
+            this.Text = "PassWord";
+            this.Load += new System.EventHandler(this.PassWordWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelPass;
+        private System.Windows.Forms.TextBox textBoxPass;
+        private System.Windows.Forms.Button buttonPass;
+        private System.Windows.Forms.Label labelError;
+        private System.Windows.Forms.Button buttonCreate;
+    }
+}
---
+++ PassWordWindow.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBAutoServise
+{
+    public partial class PassWordWindow : Form
+    {
+        MainWindow MainWindow = new MainWindow();
+        public bool FirstPassword { get; set; } = false;
+        public PassWordWindow()
+        {
+            InitializeComponent();
+        }
+        private void PassWordWindow_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ButtonPass_Click(object sender, EventArgs e)
+        {
+            string passWord = textBoxPass.Text;
+            MainWindow.Table = new DataTable();
+            MainWindow.Connection = new SqlConnection(MainWindow.DataSource);
+            MainWindow.Command = new SqlCommand("SELECT * FROM PassWords WHERE PassWord = @key", MainWindow.Connection);
+            MainWindow.Command.Parameters.Add("@key", SqlDbType.VarChar).Value = passWord;
+            MainWindow.Adapter = new SqlDataAdapter();
+            MainWindow.Adapter.SelectCommand = MainWindow.Command;
+            MainWindow.Adapter.Fill(MainWindow.Table);
+            if (MainWindow.Table.Rows.Count > 0)
+            {
+                EditingMode editingMode = new EditingMode();
+                editingMode.Show();
+                this.Close();
+            }
+            else
+            {
+                labelError.Visible = true;
+            }
+
+
+        }
+        private void ButtonCreate_Click(object sender, EventArgs e)
+        {
+            MainWindow.Table = new DataTable();
+            MainWindow.Connection = new SqlConnection(MainWindow.DataSource);
+            MainWindow.Command = new SqlCommand("SELECT * FROM PassWords", MainWindow.Connection);
+            MainWindow.Adapter = new SqlDataAdapter();
+            MainWindow.Adapter.SelectCommand = MainWindow.Command;
+            MainWindow.Adapter.Fill(MainWindow.Table);
+            if (MainWindow.Table.Rows.Count == 0)
+            {
+                FirstPassword = true;
+                CreatePass createPass = new CreatePass();
+                createPass.Show();
+                this.Close();
+            }
+            else
+            {
+                PasswordConfirmation confirmation = new PasswordConfirmation();
+                confirmation.Show();
+                this.Close();
+            }
+        }
+
+        private void TextBoxPass_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            MainWindow.ForbiddenInputs(e);
+        }
+    }
+}
